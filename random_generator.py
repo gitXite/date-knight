1a1ef9cd47e4b7077531e4ad6cf5454c373759fb
Daniel <Daniel.halaas@gmail.com>
2024-11-11 17:16:28 +0100

    Create random_generator.py

---
+++ random_generator.py
@@ -0,0 +1,32 @@
+from random import random
+
+
+restaurants = [
+    "mcdonalds",
+    "burger_king"
+]
+
+activities = [
+    "movie",
+    "mini_golf"
+]
+
+movies = [
+    "saw",
+    "cinderella"
+]
+
+dates = {
+    "restaurant": random.restaurants,
+    "activity": random.activities,
+    "movie": random.movies
+}
+
+def get_date(dates):
+    restaurant = pairing["restaurant"]
+    activity = pairing["activity"]
+    movie = None
+    if activity == "movie":
+        movie = pairing["movie"]
+        return f"----------\nYour selected date:\nRestaurant: {restaurant}\nMovie: {movie}\n----------"
+    return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\n----------"
