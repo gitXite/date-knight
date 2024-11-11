acf163516f1231290fb82e0d5eaa0065d37c00dd
Daniel <Daniel.halaas@gmail.com>
2024-11-11 18:57:56 +0100

    Update random_generator.py

--- random_generator.py
+++ random_generator.py
@@ -2,18 +2,17 @@ import random
 from lists import restaurants, activities, movies
 
 
-dates = {
+date_details = {
     "restaurant": random.choice(restaurants),
     "activity": random.choice(activities),
-    "movie": random.choice(movies)
+    "film": random.choice(movies)
 }
 
-def get_date(dates):
-    restaurant = dates["restaurant"]
-    activity = dates["activity"]
-    movie = None
-    if activity == "movie":
-        movie = dates["movie"]
-        return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\nMovie suggestion: {movie}\n----------"
+def get_date(date_details):
+    restaurant = date_details["restaurant"]
+    activity = date_details["activity"]
+    movie = date_details["film"]
+    if activity == "film":
+        return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\nFilm suggestion: {movie}\n----------"
     else:
         return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\n----------"
