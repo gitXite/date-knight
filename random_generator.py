33a776452a9aa7453f43158232b6bcec969d28db
Daniel <Daniel.halaas@gmail.com>
2024-11-11 17:43:42 +0100

    Update random_generator.py

--- random_generator.py
+++ random_generator.py
@@ -15,4 +15,5 @@ def get_date(dates):
     if activity == "movie":
         movie = dates["movie"]
         return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\nMovie suggestion: {movie}\n----------"
-    return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\n----------"
+    else:
+        return f"----------\nYour selected date:\nRestaurant: {restaurant}\nActivity: {activity}\n----------"
