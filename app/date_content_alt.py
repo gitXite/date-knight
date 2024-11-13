e762078b975b6e41d2c6914cff4bc37d64a474d7
Daniel <Daniel.halaas@gmail.com>
2024-11-13 17:21:56 +0100

    Create date_content_alt.py

---
+++ app/date_content_alt.py
@@ -0,0 +1,249 @@
+activities_bergen = {
+    "Movie": {
+        "genre": {
+            "Action": {},
+            "Thriller": {},
+            "Romance": {},
+            "Comedy": {},
+            "Horror": {},
+            "Science fiction": {},
+            "Documentary": {},
+            "Mystery": {}
+        }
+    },
+    "Restaurant": {
+        "Villani": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Varanasi": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Jest": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }, 
+        "Lola": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Amici Miei": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Sumo": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Olivia": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Hekkan Burger": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Villa Paradiso": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Banzha":{
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Bjerck": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    },
+    "Homemade dinner": {
+        "Marry me chicken": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Sweet & sour chicken with cashew bed": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Pasta carbonara": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Steak": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    },
+    "Drinks": {
+        "Last Monkey": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "No Stress": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Jest": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }, 
+        "Dark & Stormy": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Magda": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "HEM": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Ginial Bar": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Café Opera": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Kjuagutt": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Folk & Røvere": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Lampemannen Bar": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Løvetann Café & Bistro": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    },
+    "Bar games": {
+        "Bar 3": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Inside Rock Cafe": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "**Den nye på engen**": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    },
+    "Run/hike": {
+        "Stoltzekleiven": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Ulrikken": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Vidden": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }, 
+        "Fløyen": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Løvstakken": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        },
+        "Store Lungegårdsvannet": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    }, 
+    "Jigsaw": {
+        "Time": "N/A",
+        "Cost": "~ 500,-"
+    },
+    "Paint": {
+        "Time": "1-3 hours",
+        "Cost": "~ 200,-"
+    },
+    "Stand up": {
+        "Time": "2 hours",
+        "Cost": "~ 100-700,-",
+        "Link": "INSERT URL HERE"
+    },
+    "Mini golf": {
+        "Time": "1-2 hours",
+        "Cost": "N/A",
+        "Link": "INSERT URL HERE"
+    },
+    "Dessert": {
+        "Placeholder": {
+            "Time": "",
+            "Cost": "",
+            "Link": "INSERT URL HERE"
+        }
+    }, 
+    "Theatre": {
+        "Time": "",
+        "Cost": "",
+        "Link": "INSERT URL HERE"
+    },
+    "Cinema": {
+        "Time": "2:30 hours",
+        "Cost": "~ 300,-",
+        "Link": "INSERT URL HERE"
+    },
+    "Paint'n Sip": {
+        "Time": "N/A",
+        "Cost": "~ 100-400,-",
+        "Link": "INSERT URL HERE"
+    },
+    "Sauna": {
+        "Time": "0:50-2 hours",
+        "Cost": "~ 250,-",
+        "Link": "INSERT URL HERE"
+    }
+}
