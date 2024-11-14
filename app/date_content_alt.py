a330875d2cf52cd88ecf356ffb0a5cc16a8b7f04
gitXite <daniel.halaas@gmail.com>
2024-11-14 19:55:58 +0100

    Completed Stavanger

--- app/date_content_alt.py
+++ app/date_content_alt.py
@@ -292,3 +292,290 @@ activities_bergen = {
         {"Link": "INSERT URL HERE"}
     ]
 }
+
+activities_stavanger = {
+    "Movie": {
+        "genre": {
+            "Action": {},
+            "Thriller": {},
+            "Romance": {},
+            "Comedy": {},
+            "Horror": {},
+            "Science fiction": {},
+            "Documentary": {},
+            "Mystery": {}
+        }
+    },
+    "Restaurant": {
+        "Sumo": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Olivia": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Døgnvill": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Hekkan Burger": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Brasserie Pigalle": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Pasha": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Yips": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Jai Hind": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Zouq": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Villa 22": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Matbaren Bistro by Renaa": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Sirkus Renaa": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Homemade dinner": {
+        "Marry me chicken": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Sweet & sour chicken with cashew bed": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Pasta carbonara": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Steak": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Drinks": {
+        "Bøker og Børst": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Pjolter og Punsj": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Hanekam": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "No Stress": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Bardello": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Aloha": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Valhalla Bar": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Lervig Local": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "På Kornet": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Bar game": {
+        "Lucky Bowl & Lounge": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ],
+        "Inside Rock Cafe": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    },
+    "Jigsaw": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Paint": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Stand up": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Stavanger Camping": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Dessert": {
+        "Placeholder": [
+            {"Time": ""},
+            {"Cost": ""},
+            {"Link": "INSERT URL HERE"}
+        ]
+    }, 
+    "Theatre": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Cinema": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Paint'n Sip": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Pottery class": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Cooking class": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Dance lessons": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Spa": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Sup/kayak": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Picnic": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Hammock": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ],
+    "Bouldering": [
+        {"Time": ""},
+        {"Cost": ""},
+        {"Link": "INSERT URL HERE"}
+    ]
+}
+
+activities_oslo = {
+    "Movie": {
+        "genre": {
+            "Action": [],
+            "Thriller": [],
+            "Romance": [],
+            "Comedy": [],
+            "Horror": [],
+            "Science fiction": [],
+            "Documentary": [],
+            "Mystery": []
+        }
+    },
+    "Oslo Camping": None,
+    "Jigsaw": None,
+    "Paint": None,
+    "Stand up": None,
+    "Dessert": None, 
+    "Theatre": None,
+    "Cinema": None,
+    "Paint'n Sip": None,
+    "Sauna": None
+}
+
+activities_trondheim = {
+    "Movie": {
+        "genre": {
+            "Action": [],
+            "Thriller": [],
+            "Romance": [],
+            "Comedy": [],
+            "Horror": [],
+            "Science fiction": [],
+            "Documentary": [],
+            "Mystery": []
+        }
+    },
+    "Trondheim Camping": None,
+    "Jigsaw": None,
+    "Paint": None,
+    "Stand up": None,
+    "Dessert": None, 
+    "Theatre": None,
+    "Cinema": None,
+    "Paint'n Sip": None
+}
