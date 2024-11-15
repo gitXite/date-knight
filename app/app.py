2b69c5d48f442da0eb4c5acacf4faca8783c486b
Daniel <Daniel.halaas@gmail.com>
2024-11-15 15:12:05 +0100

    Fixed bug

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, jsonify
-from main_logic import *
-from app.random_contents import *
+from main_logic import get_random_activities, get_city, get_activities_by_category, get_number_activities
+from random_contents import *
 
 
 app = Flask(__name__)
