4a2313f8b6d28ff2bb46b60cb0ee7baab85e98cc
gitXite <daniel.halaas@gmail.com>
2024-11-15 12:01:28 +0100

    Added app.py with Flask framework

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, render_template, jsonify
+from main_logic import *
+from date_contents import *
+
+
+app = Flask(__name__)
+
+    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
