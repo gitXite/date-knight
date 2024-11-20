bf1b8ef1f6c10c71a7903b1c4dd620accd9e4070
Daniel <Daniel.halaas@gmail.com>
2024-11-20 15:14:36 +0100

    Added render templates for different pages

--- app/app.py
+++ app/app.py
@@ -7,14 +7,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def base():
     return render_template('base.html')
 
-@app.route('/#about')
+@app.route('/app')
+def index():
+    return render_template('app.html')
+
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/#contact')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
