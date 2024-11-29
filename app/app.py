f668cca91d75c8a4f82c0469995597cb2f23e7a4
gitXite <daniel.halaas@gmail.com>
2024-11-29 10:37:09 +0100

    Added routes for login and register

--- app/app.py
+++ app/app.py
@@ -22,6 +22,14 @@ def contact():
 def about():
     return render_template('about.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
